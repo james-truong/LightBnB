545d49ec2fb068f5fc0bf8f2fce5a6dce64532a3
james-truong <james.truong@hotmail.com>
2019-11-14 20:31:18 +0000

    refactored db module

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,14 +1,6 @@
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
-
-const { Pool } = require("pg");
-
-const pool = new Pool({
-	user: "vagrant",
-	password: "123",
-	host: "localhost",
-	database: "lightbnb"
-});
+const properties = require('./json/properties.json');
+const users = require('./json/users.json');
+const db = require('./db.js');
 
 /// Users
 
@@ -18,8 +10,7 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-	return pool
-		.query(
+	return db.query(
 			`
   SELECT *
   FROM users
@@ -37,7 +28,7 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return pool.query(`
+  return db.query(`
   SELECT * 
   FROM users
   WHERE id = $1;`, [id])
@@ -53,7 +44,7 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  return pool.query(
+  return db.query(
   `INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3)
   RETURNING *;`, [user.name, user.email, user.password])
@@ -69,8 +60,7 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-	return pool
-		.query(
+	return db.query(
 			`
   SELECT reservations.*, properties.*, average_sub.average_rating as average_rating
   FROM reservations 
@@ -160,7 +150,7 @@ const getAllProperties = function(options, limit = 10) {
 	}
 
 	console.log(queryString, queryParams);
-	return pool.query(queryString, queryParams).then(res => res.rows);
+	return db.query(queryString, queryParams).then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
@@ -170,7 +160,7 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty =  function(property) {
-  return pool.query(`  INSERT INTO properties 
+  return db.query(`  INSERT INTO properties 
   (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
     
   VALUES (
---
+++ LightBnB_WebApp-master/server/db.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+module.exports = {
+  query: (text, params) => {
+    const start = Date.now();
+    return pool.query(text, params)
+    .then(res => {
+      const duration = Date.now() - start;
+      console.log('executed query', { text, params, duration, rows: res.rowCount });
+      return res;
+    }).catch(err => console.error('query error', err.stack));
+  },
+};
