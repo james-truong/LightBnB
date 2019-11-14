1470b4cc339930b83d74b31816c2d8ec43341c90
james-truong <james.truong@hotmail.com>
2019-11-14 16:34:29 +0000

    added getAllreservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,12 +1,13 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
-const { Pool } = require('pg');
+const properties = require("./json/properties.json");
+const users = require("./json/users.json");
+
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
+	user: "vagrant",
+	password: "123",
+	host: "localhost",
+	database: "lightbnb"
 });
 
 /// Users
@@ -26,7 +27,7 @@ const getUserWithEmail = function(email) {
   `,
 			[email]
 		)
-		.then(console.log(res));
+		.then(res => (res.rows[0]));
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -68,8 +69,25 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+	return pool
+		.query(
+			`
+  SELECT reservations.*, properties.*, average_sub.average_rating as average_rating
+  FROM reservations 
+  JOIN properties ON properties.id = property_id
+  JOIN
+    (SELECT property_reviews.property_id, AVG(property_reviews.rating) as average_rating
+    FROM property_reviews
+    GROUP BY property_reviews.property_id) as average_sub 
+    ON average_sub.property_id = properties.id
+  WHERE end_date < now()::date AND guest_id = $1
+  ORDER BY start_date
+  LIMIT $2;
+  `,
+			[guest_id, limit]
+		)
+		.then(res => res.rows);
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
